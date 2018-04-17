e27296448fdeaf094242e2a4f6e1136ea855fa8e
Mike Cluck <mcluck90@gmail.com>
2018-04-17 09:05:46 -0600

    Removed test code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
 extern crate win_opacity;
-use win_opacity::*;
 
 fn main() {
-	for win in get_visible_windows() {
-		if get_window_title(win).contains("Firefox") {
-			println!("{}", get_window_title(win));
-			set_opacity(win, 220);
-		}
-	}
+	println!("Command line interface: coming soon");
 }
